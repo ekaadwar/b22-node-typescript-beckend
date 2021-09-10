c6a52ac2ca2d680ae94f397e1a0045c1d17d98df
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-09-10 08:39:58 +0700

    encrypt the password

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { createUser, getUserByEmail } from '../models/users.model'
+import argon from 'argon2'
 
 export const login = async (req: Request, res: Response) => {
   const { email } = req.body
@@ -17,7 +18,8 @@ export const register = async (req: Request, res: Response) => {
   const user = await getUserByEmail(email)
 
   if (user.length < 1) {
-    const result = await createUser({ email, password })
+    const encPassword = await argon.hash(password)
+    const result = await createUser({ email, password: encPassword })
     return res.json({
       success: true,
       message: 'Register successfully',
