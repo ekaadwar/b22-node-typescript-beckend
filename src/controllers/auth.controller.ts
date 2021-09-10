b16fe0dfa9ad3e0a8a3edac108bf2710facced8c
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-09-10 09:08:05 +0700

    compare the password; use jsonwebtoken to get the token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,15 +1,31 @@
 import { Request, Response } from 'express'
 import { createUser, getUserByEmail } from '../models/users.model'
 import argon from 'argon2'
+import jwt from 'jsonwebtoken'
 
 export const login = async (req: Request, res: Response) => {
-  const { email } = req.body
+  const { email, password } = req.body
   const result = await getUserByEmail(email)
-  // console.log(result)
-
+  // console.log(result[0])
+  if (result.length > 0) {
+    const user = result[0]
+    try {
+      const compare = await argon.verify(user.password, password)
+      return res.json({
+        success: true,
+        message: 'Login success',
+        token: jwt.sign({ id: user.id }, process.env.APP_KEY || ''),
+      })
+    } catch (e) {
+      return res.json({
+        success: false,
+        message: 'Wrong email or password',
+      })
+    }
+  }
   return res.json({
-    success: true,
-    message: 'Login success',
+    success: false,
+    message: 'Email not found',
   })
 }
 
